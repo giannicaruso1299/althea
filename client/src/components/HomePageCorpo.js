91a5d04fa99fa3fb680c3bf649765dfe6d72fb1e
Giovan Battista Caruso <giannicaruso1897@gmail.com>
2021-02-20 14:27:21 +0100

    Aggiunte immagini nella homepage

--- client/src/components/HomePageCorpo.js
+++ client/src/components/HomePageCorpo.js
@@ -8,28 +8,56 @@ function HomePageCorpo() {
         <div className="container-fluid mb-5">
             <Affiliati/>
             <Header/>
-            {/*xl*/}
-            {/*<div className="container-fluid mt-3 d-none d-xl-block" id="body">*/}
-            {/*    <div className="row">*/}
-            {/*        <div className="col-8 ml-5" id="section">*/}
-            {/*            <img src={require('../img/confetti.jpg')} className="image"></img>*/}
-            {/*        </div>*/}
-            {/*        <div className="col-3 ml-4" id="sidebar">*/}
-
-            {/*        </div>*/}
-            {/*    </div>*/}
-            {/*</div>*/}
-            {/*da sm a xl*/}
-            {/*<div className="container mt-3 d-xl-none d-block" id="body-resp">*/}
-            {/*    <div className="row">*/}
-            {/*        <div className="col-12" id="section">*/}
-            {/*            <img src={require('../img/confetti.jpg')} className="image"></img>*/}
-            {/*        </div>*/}
-            {/*    </div>*/}
-            {/*</div>*/}
-            {/*<div className="container-fluid position-relative">*/}
-            {/*    <Footer/>*/}
-            {/*</div>*/}
+            <div className="row mt-3 justify-content-center mx-1">
+                <h3 className="mb-3">Testo prima parte homepage</h3>
+                <div id="carouselExampleSlidesOnly" className="carousel slide" data-ride="carousel">
+                    <div className="carousel-inner">
+                        <div className="carousel-item active">
+                            <img className="d-block w-100" src={require('../img/foto_homepage/1.png')} alt="First slide"/>
+                        </div>
+                        <div className="carousel-item">
+                            <img className="d-block w-100" src={require('../img/foto_homepage/babyunicornad.jpg')} alt="Second slide"/>
+                        </div>
+                        <div className="carousel-item">
+                            <img className="d-block w-100" src={require('../img/foto_homepage/cuoreconsacrafamigliadeboracarlucci.jpg')} alt="Third slide"/>
+                        </div>
+                        <div className="carousel-item">
+                            <img className="d-block w-100" src={require('../img/foto_homepage/cab69506cm5cab69507.jpg')} alt="Third slide"/>
+                        </div>
+                        <div className="carousel-item">
+                            <img className="d-block w-100" src={require('../img/foto_homepage/cab69565b20x13xh175.jpg')} alt="Third slide"/>
+                        </div>
+                        <div className="carousel-item">
+                            <img className="d-block w-100" src={require('../img/foto_homepage/dc6326.jpg')} alt="Third slide"/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="row mt-3 justify-content-center mx-1">
+                <h3 className="mb-3">Testo seconda parte homepage</h3>
+                <div id="carouselExampleSlidesOnly" className="carousel slide" data-ride="carousel">
+                    <div className="carousel-inner">
+                        <div className="carousel-item active">
+                            <img className="d-block w-100" src={require('../img/foto_homepage/ec0f217522ed4f80915c33bccea8b841.jpg')} alt="First slide"/>
+                        </div>
+                        <div className="carousel-item">
+                            <img className="d-block w-100" src={require('../img/foto_homepage/img_1933.jpg')} alt="Second slide"/>
+                        </div>
+                        <div className="carousel-item">
+                            <img className="d-block w-100" src={require('../img/foto_homepage/lineaderutadolcicose.jpg')} alt="Third slide"/>
+                        </div>
+                        <div className="carousel-item">
+                            <img className="d-block w-100" src={require('../img/foto_homepage/lineahomefragrancedeboracarlucci.jpg')} alt="Third slide"/>
+                        </div>
+                        <div className="carousel-item">
+                            <img className="d-block w-100" src={require('../img/foto_homepage/lineasolidaleincucinacabim5.jpg')} alt="Third slide"/>
+                        </div>
+                        <div className="carousel-item">
+                            <img className="d-block w-100" src={require('../img/foto_homepage/mod1ad.jpg')} alt="Third slide"/>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
