ac27d460a57bbbe5753b1b0e51a91cff98f4f1fb
Giovan Battista Caruso <giannicaruso1897@gmail.com>
2021-02-20 14:54:08 +0100

    Sistemata dimensione immagini e aggiunte

--- client/src/components/HomePageCorpo.js
+++ client/src/components/HomePageCorpo.js
@@ -16,19 +16,19 @@ function HomePageCorpo() {
                             <img className="d-block w-100" src={require('../img/foto_homepage/1.png')} alt="First slide"/>
                         </div>
                         <div className="carousel-item">
-                            <img className="d-block w-100" src={require('../img/foto_homepage/babyunicornad.jpg')} alt="Second slide"/>
+                            <img className="d-block w-100" src={require('../img/foto_homepage/2.png')} alt="Second slide"/>
                         </div>
                         <div className="carousel-item">
-                            <img className="d-block w-100" src={require('../img/foto_homepage/cuoreconsacrafamigliadeboracarlucci.jpg')} alt="Third slide"/>
+                            <img className="d-block w-100" src={require('../img/foto_homepage/3.png')} alt="Third slide"/>
                         </div>
                         <div className="carousel-item">
-                            <img className="d-block w-100" src={require('../img/foto_homepage/cab69506cm5cab69507.jpg')} alt="Third slide"/>
+                            <img className="d-block w-100" src={require('../img/foto_homepage/4.png')} alt="Third slide"/>
                         </div>
                         <div className="carousel-item">
-                            <img className="d-block w-100" src={require('../img/foto_homepage/cab69565b20x13xh175.jpg')} alt="Third slide"/>
+                            <img className="d-block w-100" src={require('../img/foto_homepage/5.png')} alt="Third slide"/>
                         </div>
                         <div className="carousel-item">
-                            <img className="d-block w-100" src={require('../img/foto_homepage/dc6326.jpg')} alt="Third slide"/>
+                            <img className="d-block w-100" src={require('../img/foto_homepage/6.png')} alt="Third slide"/>
                         </div>
                     </div>
                 </div>
@@ -38,22 +38,22 @@ function HomePageCorpo() {
                 <div id="carouselExampleSlidesOnly" className="carousel slide" data-ride="carousel">
                     <div className="carousel-inner">
                         <div className="carousel-item active">
-                            <img className="d-block w-100" src={require('../img/foto_homepage/ec0f217522ed4f80915c33bccea8b841.jpg')} alt="First slide"/>
+                            <img className="d-block w-100" src={require('../img/foto_homepage/7.png')} alt="First slide"/>
                         </div>
                         <div className="carousel-item">
-                            <img className="d-block w-100" src={require('../img/foto_homepage/img_1933.jpg')} alt="Second slide"/>
+                            <img className="d-block w-100" src={require('../img/foto_homepage/8.png')} alt="Second slide"/>
                         </div>
                         <div className="carousel-item">
-                            <img className="d-block w-100" src={require('../img/foto_homepage/lineaderutadolcicose.jpg')} alt="Third slide"/>
+                            <img className="d-block w-100" src={require('../img/foto_homepage/9.png')} alt="Third slide"/>
                         </div>
                         <div className="carousel-item">
-                            <img className="d-block w-100" src={require('../img/foto_homepage/lineahomefragrancedeboracarlucci.jpg')} alt="Third slide"/>
+                            <img className="d-block w-100" src={require('../img/foto_homepage/10.png')} alt="Third slide"/>
                         </div>
                         <div className="carousel-item">
-                            <img className="d-block w-100" src={require('../img/foto_homepage/lineasolidaleincucinacabim5.jpg')} alt="Third slide"/>
+                            <img className="d-block w-100" src={require('../img/foto_homepage/11.png')} alt="Third slide"/>
                         </div>
                         <div className="carousel-item">
-                            <img className="d-block w-100" src={require('../img/foto_homepage/mod1ad.jpg')} alt="Third slide"/>
+                            <img className="d-block w-100" src={require('../img/foto_homepage/12.png')} alt="Third slide"/>
                         </div>
                     </div>
                 </div>
