192369f8a9a27deb9db35516ca057d79d5a84a40
Giovan Battista Caruso <giannicaruso1897@gmail.com>
2021-02-24 23:37:48 +0100

    Aggiustato il footer

--- client/src/components/EventsCarousel.js
+++ client/src/components/EventsCarousel.js
@@ -3,7 +3,8 @@ import React from 'react';
 function EventCarousel() {
     return (
         <div>
-            <div className="my-4 justify-content-center">
+            {/**lg only**/}
+            <div className="row my-4 justify-content-center d-md-block d-none" id="carousel-container">
                 <div id="carouselExampleDark" className="carousel carousel-dark slide" data-bs-ride="carousel">
                     <div className="carousel-indicators">
                         <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0"
@@ -15,25 +16,71 @@ function EventCarousel() {
                     </div>
                     <div className="carousel-inner">
                         <div className="carousel-item active" data-bs-interval="10000">
-                            <img src={require('../img/sfondo3.jpg')} className="d-block w-100" alt="..." style={{height:"250px"}}/>
+                            <img src={require('../img/sfondo3.jpg')} className="d-block w-100" alt="..."/>
                                 <div className="carousel-caption d-none d-md-block">
                                     <h5>First slide label</h5>
                                     <p>Some representative placeholder content for the first slide.</p>
                                 </div>
                         </div>
                         <div className="carousel-item" data-bs-interval="2000">
-                            <img src={require('../img/sfondo2.jpg')} className="d-block w-100" alt="..." style={{height:"250px"}}/>
+                            <img src={require('../img/sfondo2.jpg')} className="d-block w-100" alt="..."/>
                                 <div className="carousel-caption d-none d-md-block">
                                     <h5>Second slide label</h5>
                                     <p>Some representative placeholder content for the second slide.</p>
                                 </div>
                         </div>
                         <div className="carousel-item">
-                            <img src={require('../img/sfondo4.png')} className="d-block w-100" alt="..." style={{height:"250px"}}/>
+                            <img src={require('../img/sfondo4.png')} className="d-block w-100" alt="..."/>
                                 <div className="carousel-caption d-none d-md-block">
                                     <h5>Third slide label</h5>
                                     <p>Some representative placeholder content for the third slide.</p>
                                 </div>
+                        </div>
+                    </div>
+                    <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark"
+                            data-bs-slide="prev">
+                        <span className="carousel-control-prev-icon" aria-hidden="true"/>
+                        <span className="visually-hidden">Previous</span>
+                    </button>
+                    <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleDark"
+                            data-bs-slide="next">
+                        <span className="carousel-control-next-icon" aria-hidden="true"/>
+                        <span className="visually-hidden">Next</span>
+                    </button>
+                </div>
+            </div>
+            {/**sm only*/}
+            <div className="row my-4 justify-content-center d-md-none d-block" id="carousel-container-sm">
+                <div id="carouselExampleDark" className="carousel carousel-dark slide" data-bs-ride="carousel">
+                    <div className="carousel-indicators">
+                        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0"
+                                className="active" aria-current="true" aria-label="Slide 1"/>
+                        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1"
+                                aria-label="Slide 2"/>
+                        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2"
+                                aria-label="Slide 3"/>
+                    </div>
+                    <div className="carousel-inner">
+                        <div className="carousel-item active" data-bs-interval="10000">
+                            <img src={require('../img/sfondo3.jpg')} className="d-block w-100" alt="..."/>
+                            <div className="carousel-caption d-none d-md-block">
+                                <h5>First slide label</h5>
+                                <p>Some representative placeholder content for the first slide.</p>
+                            </div>
+                        </div>
+                        <div className="carousel-item" data-bs-interval="2000">
+                            <img src={require('../img/sfondo2.jpg')} className="d-block w-100" alt="..."/>
+                            <div className="carousel-caption d-none d-md-block">
+                                <h5>Second slide label</h5>
+                                <p>Some representative placeholder content for the second slide.</p>
+                            </div>
+                        </div>
+                        <div className="carousel-item">
+                            <img src={require('../img/sfondo4.png')} className="d-block w-100" alt="..."/>
+                            <div className="carousel-caption d-none d-md-block">
+                                <h5>Third slide label</h5>
+                                <p>Some representative placeholder content for the third slide.</p>
+                            </div>
                         </div>
                     </div>
                     <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark"
--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -5,28 +5,38 @@ function Footer() {
     return (
         <div className="mt-5 pb-3" id="myFooter">
             <div className="container-fluid">
-                <div className="row justify-content-sm-around justify-content-center">
-                    <div className="mt-4 text-center text-sm-left">
-                        <h4 className="h2 font-weight-bold normal-font">Contatti</h4>
-                        <hr className="bg-dark mx-sm-0 mx-3"/>
-                        <small>Althea Bomboniere di Caruso Adelfio</small>
-                        <br/>
-                        <small>Via Bologna 94/a</small>
-                        <br/>
-                        <small>Partinico, PA</small>
-                        <br/>
-                        <small>Email: <a href="mailto:althea@altheabomboniere.it" id="mail">althea@ altheabomboniere.it</a></small>
-                        <br/>
-                        <small>Telefono: <a href="tel:3280886172" id="phone">328 0886172</a></small>
-                    </div>
-                    <div className="mt-4 text-center">
-                        <h4 className="h2 text-center font-weight-bold normal-font">Veniteci a trovare!</h4>
-                        <hr className="bg-dark"/>
-                        <a rel="noopener noreferrer" href="https://www.google.com/maps/place/Caruso+Adelfio/@38.0412491,13.110435,15z/data=!4m8!1m2!2m1!1salthea+bomboniere+partinico!3m4!1s0x13198db2eab368b9:0x9beb6a033d5b2704!8m2!3d38.0412491!4d13.1191897" target="_blank">
-                            <img src={require("../img/maps.png")} className="g-maps" alt="Google Maps"/>
-                        </a>
+                <div className="jumbotron-fluid justify-content-md-around">
+                    <div className="row">
+                        <div className="col-md-6 col-12">
+                            <div className="text-center">
+                                <h4 className="h2 pt-md-5 pt-3 font-weight-bold normal-font">Contatti</h4>
+                                <hr className="mx-md-0 mx-3"/>
+                                <p>Althea Bomboniere di Caruso Adelfio</p>
+                                <p>Via Bologna 94/A, Partinico (PA)</p>
+                                <p>Email: <a href="mailto:althea@altheabomboniere.it" id="mail">althea@altheabomboniere.it</a></p>
+                                <p>Telefono: <a href="tel:3280886172" id="phone">328 0886172</a></p>
+                            </div>
+                        </div>
+                        <div className="col-md-6 col-12">
+                            <div className="text-center">
+                                <h4 className="h2 pt-md-5 pt-3 font-weight-bold normal-font">Veniteci a trovare!</h4>
+                                <hr className="mx-md-0 mx-3"/>
+                                <a rel="noopener noreferrer" href="https://www.google.com/maps/place/Caruso+Adelfio/@38.0412491,13.110435,15z/data=!4m8!1m2!2m1!1salthea+bomboniere+partinico!3m4!1s0x13198db2eab368b9:0x9beb6a033d5b2704!8m2!3d38.0412491!4d13.1191897" target="_blank">
+                                    <img src={require("../img/maps.png")} className="g-maps" alt="Google Maps"/>
+                                </a>
+                            </div>
+                        </div>
                     </div>
                 </div>
+                {/*<div className="row justify-content-sm-around justify-content-center">*/}
+                {/*    <div className="mt-4 text-center">*/}
+                {/*        <h4 className="h2 text-center font-weight-bold normal-font">Veniteci a trovare!</h4>*/}
+                {/*        <hr className="bg-dark"/>*/}
+                {/*        <a rel="noopener noreferrer" href="https://www.google.com/maps/place/Caruso+Adelfio/@38.0412491,13.110435,15z/data=!4m8!1m2!2m1!1salthea+bomboniere+partinico!3m4!1s0x13198db2eab368b9:0x9beb6a033d5b2704!8m2!3d38.0412491!4d13.1191897" target="_blank">*/}
+                {/*            <img src={require("../img/maps.png")} className="g-maps" alt="Google Maps"/>*/}
+                {/*        </a>*/}
+                {/*    </div>*/}
+                {/*</div>*/}
             </div>
         </div>
     );
