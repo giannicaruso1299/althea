50f7b2ba56ab031137e5aa9d68dcf0ff3b8e8675
Gianni <giannicaruso1897@gmail.com>
2021-04-02 22:19:17 +0200

    Manca solo il path delle immagini

--- client/src/components/ConfettiAll.js
+++ client/src/components/ConfettiAll.js
@@ -13,7 +13,7 @@ const ConfettiAll = () => {
     const [itemsPerPage] = useState(12);
     const [noItems, setNoItems] = useState(false);
 
-    let path="http://althea-bomboniere.it/api/";
+    let path="http://althea-bomboniere.it:5000/";
 
     useEffect(() => {
         const fetchItems = async () => {
